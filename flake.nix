424ee8798f066f69754f116389ac3d09793b8704
jiriks74 <jiri@stefka.eu>
2025-03-02 22:22:19 +0100

    fix: Formatting

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     EDMarketConnector-src = {
-        url = "github:EDCD/EDMarketConnector?ref=Release/5.12.2";
-        flake = false;
+      url = "github:EDCD/EDMarketConnector?ref=Release/5.12.2";
+      flake = false;
     };
     pyproject-nix = {
       url = "github:nix-community/pyproject.nix";
@@ -13,29 +13,31 @@
     };
   };
 
-  outputs = { nixpkgs, EDMarketConnector-src, pyproject-nix, self, ... }:
-    let
-      inherit (nixpkgs) lib;
+  outputs = {
+    nixpkgs,
+    EDMarketConnector-src,
+    pyproject-nix,
+    self,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
 
-      EDMarketConnector = pyproject-nix.lib.project.loadRequirementsTxt { projectRoot = EDMarketConnector-src; };
+    EDMarketConnector = pyproject-nix.lib.project.loadRequirementsTxt {projectRoot = EDMarketConnector-src;};
 
-      # This example is only using x86_64-linux
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    # This example is only using x86_64-linux
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
 
-      python = pkgs.python3Full;
-
+    python = pkgs.python3Full;
+  in {
+    formatter.x86_64-linux = pkgs.alejandra;
+    # Build our package using `buildPythonPackage`
+    packages.x86_64-linux.default = let
+      pythonEnv = python.withPackages (
+        EDMarketConnector.renderers.withPackages {inherit python;}
+      );
     in
-    {
-      # Build our package using `buildPythonPackage`
-      packages.x86_64-linux.default =
-        let
-          pythonEnv =
-            python.withPackages (
-              EDMarketConnector.renderers.withPackages { inherit python; }
-          );
-        in
-          pkgs.writeShellScriptBin "EDMarketConnector" ''
-            exec ${pythonEnv}/bin/python ${self}/EDMarketConnector.py "$@"
-          '';
-    };
+      pkgs.writeShellScriptBin "EDMarketConnector" ''
+        exec ${pythonEnv}/bin/python ${self}/EDMarketConnector.py "$@"
+      '';
+  };
 }
