b3cb4ecebf0fe68a53a65fa7533c9a229031c7a7
jiriks74 <jiri@stefka.eu>
2025-03-02 23:53:25 +0100

    feat: Add min-ed-launcher

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     EDMarketConnector-src = {
       url = "github:EDCD/EDMarketConnector?ref=Release/5.12.2";
       flake = false;
@@ -11,6 +12,11 @@
       url = "github:nix-community/pyproject.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # min-er-launcher = {
+    #   url = "github:rfvgyhn/min-ed-launcher?ref=v0.11.2";
+    #   flake = false;
+    # };
   };
 
   outputs = {
@@ -31,13 +37,16 @@
   in {
     formatter.x86_64-linux = pkgs.alejandra;
     # Build our package using `buildPythonPackage`
-    packages.x86_64-linux.default = let
-      pythonEnv = python.withPackages (
-        EDMarketConnector.renderers.withPackages {inherit python;}
-      );
-    in
-      pkgs.writeShellScriptBin "EDMarketConnector" ''
-        exec ${pythonEnv}/bin/python ${self}/EDMarketConnector.py "$@"
-      '';
+    packages.x86_64-linux = {
+      EDMarketConnector = let
+        pythonEnv = python.withPackages (
+          EDMarketConnector.renderers.withPackages {inherit python;}
+        );
+      in
+        pkgs.writeShellScriptBin "EDMarketConnector" ''
+          exec ${pythonEnv}/bin/python ${self}/EDMarketConnector.py "$@"
+        '';
+    };
+    min-ed-launcher = pkgs.callPackage ./min-ed-launcher.nix {};
   };
 }
---
+++ min-ed-launcher.nix
@@ -0,0 +1,35 @@
+{
+  lib,
+  buildDotnetModule,
+  fetchFromGitHub,
+  git,
+}:
+buildDotnetModule rec {
+  pname = "min-ed-launcher";
+  version = "0.11.2";
+
+  src = fetchFromGitHub {
+    owner = "rfvgyhn";
+    repo = "min-ed-launcher";
+    tag = "v${version}";
+    hash = "sha256-p970rsDyqXBbxo18q/ocNRkBKBq2CwJA3tAfDicrTOE=";
+    leaveDotGit = true;
+  };
+
+  projectFile = "MinEdLauncher.sln";
+  nugetDeps = ./min-ed-launcher.deps.json;
+  buildInputs = [
+    git
+  ];
+
+  executables = ["MinEdLauncher"];
+
+  meta = {
+    homepage = "https://github.com/rfvgyhn/min-ed-launcher";
+    description = "";
+    license = lib.licenses.gpl2Only;
+    platforms = lib.platforms.x86_64;
+    mainProgram = "MinEdLauncher";
+    maintainers = with lib.maintainers; [ jiriks74 ];
+  };
+}
