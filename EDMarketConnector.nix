c9e26d2c0d9f8ba8100cf059db00fcc7f2d424a2
jiriks74 <jiri@stefka.eu>
2025-03-03 07:51:20 +0100

    feat(EDMarketConnector): Convert to a proper nix package

---
+++ EDMarketConnector.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  fetchFromGitHub,
+  python3Full,
+  writeShellScriptBin
+}:
+let
+  pythonEnv = python3Full.buildEnv.override {
+    extraLibs = with python3Full.pkgs.pythonPackages; [
+      requests
+      pillow
+      watchdog
+      semantic-version
+      psutil
+    ];
+    ignoreCollisions = true;
+  };
+
+  src = fetchFromGitHub {
+    owner = "EDCD";
+    repo = "EDMarketConnector";
+    tag = "Release/5.12.2";
+    hash = "sha256-3ywu/EJdIsKqTN3uaA5F0tZK6tybl483Yiwqh7W4yCc=";
+  };
+in
+writeShellScriptBin "EDMarketConnector" ''
+  exec ${pythonEnv}/bin/python ${src}/EDMarketConnector.py "$@"
+'' // {
+  meta = {
+    homepage = "https://github.com/EDCD/EDMarketConnector";
+    description = "Downloads commodity market and other station data from the game Elite: Dangerous for use with all popular online and offline trading tools. ";
+    license = lib.licenses.gpl2Only;
+    maintainers = with lib.maintainers; [ jiriks74 ];
+    platforms = lib.platforms.x86_64;
+  };
+}
--- flake.nix
+++ flake.nix
@@ -3,50 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    EDMarketConnector-src = {
-      url = "github:EDCD/EDMarketConnector?ref=Release/5.12.2";
-      flake = false;
-    };
-    pyproject-nix = {
-      url = "github:nix-community/pyproject.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # min-er-launcher = {
-    #   url = "github:rfvgyhn/min-ed-launcher?ref=v0.11.2";
-    #   flake = false;
-    # };
   };
 
   outputs = {
     nixpkgs,
-    EDMarketConnector-src,
-    pyproject-nix,
-    self,
     ...
   }: let
-    inherit (nixpkgs) lib;
-
-    EDMarketConnector = pyproject-nix.lib.project.loadRequirementsTxt {projectRoot = EDMarketConnector-src;};
-
-    # This example is only using x86_64-linux
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
-    python = pkgs.python3Full;
   in {
     formatter.x86_64-linux = pkgs.alejandra;
-    # Build our package using `buildPythonPackage`
     packages.x86_64-linux = {
-      EDMarketConnector = let
-        pythonEnv = python.withPackages (
-          EDMarketConnector.renderers.withPackages {inherit python;}
-        );
-      in
-        pkgs.writeShellScriptBin "EDMarketConnector" ''
-          exec ${pythonEnv}/bin/python ${self}/EDMarketConnector.py "$@"
-        '';
+      EDMarketConnector = pkgs.callPackage ./EDMarketConnector.nix {};
+      min-ed-launcher = pkgs.callPackage ./min-ed-launcher.nix {};
     };
-    min-ed-launcher = pkgs.callPackage ./min-ed-launcher.nix {};
   };
 }
