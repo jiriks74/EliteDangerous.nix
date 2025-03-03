e477756d6a007bf0ed600beb0d18d294f82c014d
jiriks74 <jiri@stefka.eu>
2025-03-03 13:03:38 +0100

    fix: Fix Trakkr package

--- pkgs/trakkr.nix
+++ pkgs/trakkr.nix
@@ -1,46 +1,26 @@
-{
-  lib,
-  fetchFromGitHub,
-  buildNpmPackage,
-  makeDesktopItem,
-  nodejs_20,
-  electron_32
+{ lib, fetchFromGitHub, buildNpmPackage, makeDesktopItem, nodejs_20, electron_32
 }:
+
 buildNpmPackage rec {
   pname = "Trakkr";
-  version = "unstable-2025-03-03";
+  version = "0.1.7";
   nodejs = nodejs_20;
 
   src = fetchFromGitHub {
     owner = "skybreakdigital";
     repo = "${pname}-app";
-    rev = "edb838bd839f06cfd045d9bc2a46ed4d2533b8e1";
-    hash = "sha256-qlSnZnsMJq0sfpv8hGiJrIi1Mbiq8RhjjLgEEG03Ig0=";
+    tag = "v${version}";
+    hash = "sha256-roL0PVytPPpmkz/6yCvrtaTUhNvqvEh0/uRyPWiJj7I=";
   };
-  
+
   env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
-  
-  # patches = [
-  #   ./turn_off_dev.patch
-  # ];
-
-  makeCacheWritable = true;
-
   npmDepsHash = "sha256-qKnyFw2wnSTywKQIFADEAF/D8HsENICjqbSI4yWsZAU=";
-
-  # The prepack script runs the build script, which we'd rather do in the build phase.
-  # npmPackFlags = [ "--ignore-scripts" ];
-
-  # NODE_OPTIONS = "--openssl-legacy-provider";
-  # npmBuildScript = "build-no-pack";
 
   buildPhase = ''
     runHook preBuild
 
-    npm run build-no-pack
+    npm exec vite -- build
 
-    # NOTE: Upstream explicitly opts to not build an ASAR as it would cause all
-    # text to disappear in the app.
     npm exec electron-builder -- \
       --dir \
       -c.electronDist=${electron_32.dist} \
@@ -61,6 +41,10 @@ buildNpmPackage rec {
     cp -r ./dist/*-unpacked/{locales,resources{,.pak}} "$out/share/lib/Trakkr"
     cp ./dist/index.html "$out/share/lib/Trakkr"
 
+
+    # Code relies on checking app.isPackaged, which returns false if the executable is electron.
+    # Set ELECTRON_FORCE_IS_PACKAGED=1.
+    # https://github.com/electron/electron/issues/35153#issuecomment-1202718531
     makeWrapper '${electron_32}/bin/electron' "$out/bin/Trakkr" \
       --add-flags "$out/share/lib/Trakkr/resources/app.asar" \
       --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true}}" \
@@ -76,18 +60,15 @@ buildNpmPackage rec {
       exec = "Trakkr";
       icon = "Trakkr";
       desktopName = "Trakkr";
-      categories = [
-        "Game"
-        "Utility"
-      ];
+      categories = [ "Game" "Utility" ];
       comment = meta.description;
       terminal = false;
     })
   ];
 
   meta = {
-    broken = true;
-    description = "Trakkr is specifically designed to assist Elite Dangerous missions, offering tailored support to enhance the efficiency and management of their operations.";
+    description =
+      "Trakkr is specifically designed to assist Elite Dangerous missions, offering tailored support to enhance the efficiency and management of their operations.";
     homepage = "https://github.com/skybreakdigital/trakkr-app";
     mainProgram = "Trakkr";
     license = lib.licenses.gpl3Only;
